09d085310f6451ed496b5fe9b7a126c1516b9060
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2022-11-02 17:51:17 -0400

    random drink button now works and can cycle through various drinks

--- cocktailsapi/src/components/randomdrink.js
+++ cocktailsapi/src/components/randomdrink.js
@@ -4,7 +4,8 @@ function RandomDrink() {
     const [randomDrinkName, setRandomDrinkName] = useState()
     const [randomDrinkObject, setRandomDrinkObject] = useState()
     const [randomDrinkIngredients, setRandomDrinkIngredients] = useState('')
-
+    const [nextDrinkButton, setNextDrinkButton] = useState(false)
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'y']
     const getDrinkIngredients = (randomDrinkObject) => {
 
         let ingredientsArray = []
@@ -29,10 +30,18 @@ function RandomDrink() {
             getDrinkIngredients(data.drinks[0])
         })
 
-    }, [])
+    }, [nextDrinkButton])
 
     return (
         <div>
+            <button onClick={(event) => {
+                event.preventDefault();
+                if (nextDrinkButton) {
+                    setNextDrinkButton(false)
+                } else {
+                    setNextDrinkButton(true)
+                }
+            }}>next drink</button>
             <h1>Random Drink</h1>
             {randomDrinkObject ?
                 <div>
@@ -41,7 +50,7 @@ function RandomDrink() {
                     <h2>{randomDrinkObject.strAlcoholic}</h2>
                     <h2>{randomDrinkObject.strTags}</h2>
                     <h2> Ingredients: {randomDrinkIngredients} </h2>
-                    {randomDrinkName[0].toLowerCase() == 'a' || 'e' || 'i' || 'o' || 'u' || 'y' ? <h2>How to make an {randomDrinkName} </h2> : <h2>How to make a {randomDrinkName} </h2>}
+                    {vowels.includes(randomDrinkName[0].toLowerCase()) ? <h2>How to make an {randomDrinkName} </h2> : <h2>How to make a {randomDrinkName} </h2>}
 
                     <p>{randomDrinkObject.strInstructions}</p>
                     <pre>{JSON.stringify(randomDrinkObject, null, "\t")}</pre>
