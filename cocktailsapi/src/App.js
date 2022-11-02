93a4d687a5990fd94f0ac304676302fbaa666ba4
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2022-11-02 15:25:32 -0400

    fetched api and display drink

--- cocktailsapi/src/App.js
+++ cocktailsapi/src/App.js
@@ -1,9 +1,25 @@
-
+import { useState, useEffect } from 'react'
 
 function App() {
+    const [randomDrink, setRandomDrink] = useState()
+    const [randomDrinkObject, setRandomDrinkObject] = useState()
+
+    useEffect(() => {
+
+        fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php', {
+            mode: "cors"
+        }).then((response) => response.json())
+        .then((data) => {
+            setRandomDrinkObject(data.drinks[0])
+        })
+
+    }, [])
+
   return (
     <div className="App">
-     
+
+        <h1>Hello World</h1>
+        <pre>{JSON.stringify(randomDrinkObject, null, 4)}</pre>
     </div>
   );
 }
